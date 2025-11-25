ace0e1a78197c1031fe3de5d1c3b388d46783394
rahulrr-coder <rahulrr5002@protonmail.com>
2025-11-25 11:47:14 +0000

    Setup Project with gitignore

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.Controllers;
+[ApiController]
+[Route("[controller]")]
+
+public class WeatherController
+{
+    [HttpGet]
+    public string GetWeather()
+    {
+        return "It's sunny today";
+    }
+}
